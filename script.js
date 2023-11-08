ea330e582ba7e5c3f336e17395db8bb33756130e
Dev Dan <adesanyaboluwatife29@gmail.com>
2023-11-08 11:15:19 +0100

    Stage 1

---
+++ script.js
@@ -0,0 +1,24 @@
+alert("Hello World")
+// Variables (let, var, const)
+
+let name = "Daniel"
+// console.log(name);
+
+name = "Samuel"
+console.log(name);
+
+const myClass = "CSS"
+// console.log(myClass);
+
+// myClass = "JS"
+document.getElementById("display").innerHTML = myClass  
+console.log(myClass);
+
+//FIND ALL THE JAVASCRIPT KEYWORDS
+
+let num1 = 30
+let num2 = 50
+
+console.log(num1 + num2);
+alert(num1 + num2)
+document.getElementById("display").innerHTML = num1 + num2
